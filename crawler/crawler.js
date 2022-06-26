e475dd664632fd986b975ee0b3c2229c45357b50
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-06-26 20:16:13 +0300

    this implementation doesn't visits all the pages.

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,6 +1,8 @@
 import chalk from "chalk";
 import puppeteer from "puppeteer";
 import validator from "validator";
+import EventEmitter from 'node:events';
+import _ from 'lodash';
 
 const logger = (...messages) =>
   console.log(chalk.cyanBright("[crawler]"), ...messages);
@@ -10,9 +12,10 @@ export default class Crawler {
   #browser;
   /**@type {Set<string>} */
   #visitedPages = new Set();
+  #jobQueue = new JobQueue();
 
   async init() {
-    this.#browser = await puppeteer.launch({ headless: false });
+    this.#browser = await puppeteer.launch({ headless: true });
   }
 
   /**
@@ -21,29 +24,42 @@ export default class Crawler {
    * @returns {Promise<string[]>}
    */
   async visit(url, page) {
-    await page.goto(url);
-    this.#visitedPages.add(url);
-    const hyperlinks = await this.findHyperLinks(page);
-    await page.close();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    //i am using new URL(url).href because https://example.com/ === https://example.com should be the same.
+    this.#visitedPages.add(new URL(url).href);
+    let hyperlinks = await this.findHyperLinks(page);
+    page.close();
+    console.log(url);
+    hyperlinks = this.filterDifferentHostname(hyperlinks, new URL(url).hostname);
+    hyperlinks = this.filterOutVisitedPages(hyperlinks);
+    hyperlinks = _.uniq(hyperlinks);
+    this.#jobQueue.enqueueNJobs(hyperlinks);
+    this.#jobQueue.done(url);
     return hyperlinks;
   }
 
   /**@param {string} url */
   async crawl(url) {
     const page = await this.#browser.newPage();
+
+
+    this.#jobQueue.emitter.on('done', async () => {
+      const job = this.#jobQueue.dequeue();
+      if (job && !this.#visitedPages.has(job)) this.visit(job, await this.#browser.newPage());
+    });
+
     let hyperlinks = await this.visit(url, page);
     hyperlinks = this.filterDifferentHostname(hyperlinks, new URL(url).hostname);
-    logger({ hyperlinks });
-    const visits = [];
-    for (const hyperlink of hyperlinks) {
+    hyperlinks = this.filterOutVisitedPages(hyperlinks);
+    hyperlinks = _.uniq(hyperlinks);
+
+    const firstBatch = this.#jobQueue.enqueueNJobs(hyperlinks).dequeueNJobs(10);
+
+    for (const hyperlink of firstBatch) {
       if (!this.#visitedPages.has(hyperlink)) {
-        visits.push(this.visit(hyperlink, await this.#browser.newPage()));
+        this.visit(hyperlink, await this.#browser.newPage());
       }
     }
-    //this is to make sure that we wait for all the visit functions to end before closing the browser.
-    await Promise.all(visits);
-    logger(this.#visitedPages);
-    await this.#browser.close();
   }
 
   /**
@@ -58,6 +74,14 @@ export default class Crawler {
   }
 
   /**
+   * @param {string[]} hyperlinks
+   * @returns {string[]}
+   */
+  filterOutVisitedPages(hyperlinks) {
+    return hyperlinks.filter((hyperlink) => !this.#visitedPages.has(new URL(hyperlink).href));
+  }
+
+  /**
    * @param {import("puppeteer").Page} page 
    * @returns {Promise<string[]>} 
    **/
@@ -67,5 +91,49 @@ export default class Crawler {
       (as) => as.map((a) => a.href),
     );
     return hyperlinks.filter((link) => validator.isURL(link));
+  }
+}
+
+class JobQueue {
+  constructor() {
+    this.jobs = [];
+    this.currentProcessingJobs = new Set();
+    this.emitter = new EventEmitter();
+  }
+
+  log() {
+    console.log({ jobs: this.jobs.length, processing: this.currentProcessingJobs });
+  }
+
+  done(ID) {
+    this.currentProcessingJobs.delete(ID);
+    this.emitter.emit('done');
+  }
+
+  enqueue(job) {
+    this.jobs.push(job);
+  }
+
+  enqueueNJobs(jobs) {
+    this.jobs.push(...jobs);
+    return this;
+  }
+
+  dequeue() {
+    const ID = this.jobs.shift();
+    this.currentProcessingJobs.add(ID);
+    return ID;
+  }
+
+  dequeueNJobs(n) {
+    return this.jobs.splice(0, n);
+  }
+
+  get length() {
+    return this.jobs.length;
+  }
+
+  get isEmpty() {
+    return this.length === 0;
   }
 }
