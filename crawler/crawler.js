f2855844ff1fde0e7fa95bb510b6258956ebae95
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-08-22 00:20:44 +0300

    format

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 import validator from "validator";
-import _ from 'lodash';
-import fs from 'fs/promises';
+import _ from "lodash";
+import fs from "fs/promises";
 
 export default class Crawler {
   /** @type {import('puppeteer').Browser} browser */
@@ -11,7 +11,7 @@ export default class Crawler {
   #jobQueue = new JobQueue();
 
   async init() {
-    this.#browser = await puppeteer.launch({ headless: true });
+    this.#browser = await puppeteer.launch({ headless: false });
   }
 
   /**
@@ -21,7 +21,7 @@ export default class Crawler {
    */
   async visit(url, page) {
     await page.goto(url);
-    await page.waitForSelector('a');
+    await page.waitForSelector("a");
     //i am using new URL(url).href because https://example.com/ === https://example.com should be the same.
     this.#visitedPages.add(new URL(url).href);
     let hyperlinks = await this.findHyperLinks(page);
@@ -31,7 +31,7 @@ export default class Crawler {
 
     this.#jobQueue.enqueueNJobs(hyperlinks);
     await page.close();
-    await fs.appendFile('third-run.txt', `${url}\n`);
+    await fs.appendFile("third-run.txt", `${url}\n`);
     return hyperlinks;
   }
 
@@ -50,14 +50,17 @@ export default class Crawler {
       batch = this.#jobQueue.dequeueNJobs(10);
       console.log({
         queueLen: this.#jobQueue.length,
-        visitedSetSize: this.#visitedPages.size
+        visitedSetSize: this.#visitedPages.size,
       });
     } while (this.#jobQueue.length > 0);
     await this.#browser.close();
   }
 
   filterHyperlinks(hyperlinks, url) {
-    hyperlinks = this.filterDifferentHostname(hyperlinks, new URL(url).hostname);
+    hyperlinks = this.filterDifferentHostname(
+      hyperlinks,
+      new URL(url).hostname,
+    );
     hyperlinks = this.filterOutVisitedPages(hyperlinks);
     hyperlinks = _.uniq(hyperlinks);
     hyperlinks = this.filterOutHashUrls(hyperlinks);
@@ -79,7 +82,7 @@ export default class Crawler {
   }
 
   /**
-   * @param {string[]} hyperLinks 
+   * @param {string[]} hyperLinks
    * @param {string} hostname
    * @returns {string[]}
    */
@@ -94,13 +97,15 @@ export default class Crawler {
    * @returns {string[]}
    */
   filterOutVisitedPages(hyperlinks) {
-    return hyperlinks.filter((hyperlink) => !this.#visitedPages.has(new URL(hyperlink).href));
+    return hyperlinks.filter((hyperlink) =>
+      !this.#visitedPages.has(new URL(hyperlink).href)
+    );
   }
 
   /**
-   * @param {import("puppeteer").Page} page 
-   * @returns {Promise<string[]>} 
-   **/
+   * @param {import("puppeteer").Page} page
+   * @returns {Promise<string[]>}
+   */
   async findHyperLinks(page) {
     const hyperlinks = await page.$$eval(
       "a",
@@ -113,7 +118,9 @@ export default class Crawler {
    * @param {string[]} urls
    */
   filterOutHashUrls(urls) {
-    return urls.filter((url) => (new URL(url).hash === '') && (new URL(url).href.indexOf('#') === -1));
+    return urls.filter((url) =>
+      (new URL(url).hash === "") && (new URL(url).href.indexOf("#") === -1)
+    );
   }
 }
 
